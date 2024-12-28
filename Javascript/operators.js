60e7bf3144132ea0a94c80dc00bab89b8ecd5449
santhapriyan-s <124667344+santhapriyan-s@users.noreply.github.com>
2024-12-28 11:39:36 +0530

    first commit

---
+++ Javascript/operators.js
@@ -0,0 +1,29 @@
+//Assignment operator
+a=10
+console.log(a);
+
+//Unary operator
+a++;
+console.log(a);
+
+//Arithmetic operato
+var var1=10
+var var2=20
+var sum=var1+var2
+console.log(sum);
+
+//Relational operator
+var rel1=30
+var rel2="30"
+if(rel1==rel2){
+    console.log("Both values are same not type");
+    console.log(rel1==rel2);
+}
+else{
+    console.log("Both values are not same")
+}
+
+
+//Logical operator
+console.log(a>5 && a>5);
+console.log(a>5 || a>5);
